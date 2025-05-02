8be4091fabed323d8c13a6cabfa3809ef62580e0
Z3R0xd <matthew.farrell.d@gmail.com>
2025-05-02 13:59:43 +0700

    Real_Exercise_2

---
+++ 05_may_2025/Exercises/Real_Exercise_2.py
@@ -0,0 +1,28 @@
+class Employee:
+    def __init__(self, emp_id, emp_name, emp_salary, emp_department):
+        self.__id = emp_id
+        self.__name = emp_name
+        self.__salary = emp_salary
+        self.__department = emp_department
+        
+    def calculate_emp_salary(self, salary, hours_worked):
+        if hours_worked > 50 :
+            overtime = hours_worked-50
+            salary =+ overtime*(salary/50)
+        self.__salary = salary
+    
+    def assign_department(self, department):
+        self.__department = department
+    
+    def print_employee_details(self):
+        print(f"Name : {self.__name}\n ID : {self.__id}\n Salary : {self.__salary}\n Department : {self.__department}")
+        
+        
+Name = input("Name : ")
+ID = input("ID : ")
+Salary = int(input("Salary : "))
+Department = input("Department : ")
+
+employee = Employee(ID,Name,Salary,Department)
+
+employee.print_employee_details()
